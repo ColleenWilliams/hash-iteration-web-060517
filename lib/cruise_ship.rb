726bba86e0b047978a7dcdd0adf9a8c583a84946
github username <github email address>
2017-05-24 11:56:37 -0400

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,18 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  passenger_a = ""
+  passengers.each do |suite,passenger|
+    if suite == :suite_a && passenger[0] = "A"
+       passenger_a = passenger
+     end
+  end
+  passenger_a
 end
